8532fb0ac1bf6c19be0109ca7380ec137fbc49f2
antonMark <anton_markin98@bk.ru>
2022-01-01 19:55:02 +0300

    first task

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Написать программу, которая из имеющегося целых чисел формирует массив из чисел, больших 8.
+
+int [] array = new int [4];
+Random rnd = new Random();
+for (int i = 0; i <array.Length; i++)
+{
+    array[i] = rnd.Next(-10,20);
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine();
+
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index]>8)
+    Console.WriteLine( array[index]);
+}
+
+
+
