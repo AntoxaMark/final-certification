a5d56338c5ce0d40ec22394eade05714cdb84c65
antonMark <anton_markin98@bk.ru>
2022-01-01 19:55:37 +0300

    second task

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// 2 Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел
+
+int [] array = new int [8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9,20);
+    Console.WriteLine(array[i]);
+ 
+}
+Console.WriteLine();
+for (int index = 0; index < array.Length; index++)
+{
+    if ((array[index] % 2) == 0)
+   Console.WriteLine("Четные числа "  +array[index]);
+
+}
+
+
+
+
+
